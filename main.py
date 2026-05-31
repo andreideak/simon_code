d3700b3a02e85dcc8a250dc306b1339989197ad8
Andrei Deak <sudogong@protonmail.ch>
2026-05-31 17:44:35 +0200

    working agent mode

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import argparse
 from dotenv import load_dotenv
 from google import genai
@@ -6,7 +7,7 @@ from google.genai import types
 from prompts import system_prompt
 from call_function import available_functions, call_function
 
-def generate_content(client: genai.Client, messages: list[types.Content], verbose: bool) -> None:
+def generate_content(client: genai.Client, messages: list[types.Content], verbose: bool) -> types.GenerateContentResponse:
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages,
@@ -20,23 +21,8 @@ def generate_content(client: genai.Client, messages: list[types.Content], verbos
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-    if not response.function_calls:
-        print(f" - {response.text}")
-        return
-    
+    return response
 
-    function_responses: list[types.Part] = []
-    for function_call in response.function_calls:
-        result = call_function(function_call,verbose)
-        if (
-            not result.parts
-            or not result.parts[0].function_response
-            or not result.parts[0].function_response.response
-        ): 
-            raise RuntimeError(f"Empty function response for {function_call.name}")
-        if verbose:
-            print(f"-> {result.parts[0].function_response.response}") 
-        function_responses.append(result.parts[0])
 
 def main():
     # Use argparser to extract question from command argument
@@ -60,8 +46,41 @@ def main():
     if args.verbose:
         print(f"User prompt: {args.user_prompt}")
 
-    generate_content(client, messages, args.verbose)
-               
+    # Limit agents loop executions to 20
+    for _ in range(20):
+        response = generate_content(client, messages, args.verbose)
+
+        if not response.candidates:
+            raise RuntimeError("Gemini API response contains no .candidates property (history)")
+        for candidate in response.candidates:
+            # Append response to messsages (chat history)
+            messages.append(candidate.content)  
+        
+        if not response.function_calls:
+            print(f"Final response: {response.text}")
+            break
+    
+        function_responses: list[types.Part] = []
+        for function_call in response.function_calls:
+            result = call_function(function_call,args.verbose)
+            if (
+                not result.parts
+                or not result.parts[0].function_response
+                or not result.parts[0].function_response.response
+            ): 
+                raise RuntimeError(f"Empty function response for {function_call.name}")
+            if args.verbose:
+                print(f"-> {result.parts[0].function_response.response}") 
+            function_responses.append(result.parts[0])
+        # Append tool calls response to messages (chat history)
+        messages.append(types.Content(role="user", parts=function_responses))
+    else:
+        print("Maximum iterations reached.")
+        sys.exit(1)
+             
+        
+            
+        
 
 if __name__ == "__main__":
     main()
